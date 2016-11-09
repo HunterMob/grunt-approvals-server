0b4e660fdf725f3598b7eb595b46b142cb5c15c7
Chris Stead <cmstead@gmail.com>
2016-11-09 13:37:05 -0800

    Updated to fix callback issues in multiple grunt versions.

--- tasks/grunt-approvals-server.js
+++ tasks/grunt-approvals-server.js
@@ -7,6 +7,7 @@ var server = null;
 var runnerPath = [__dirname, '..', 'server-runner', 'runner'].join(path.sep);
 
 function start(configPath, callback) {
+    var cleanCallback = typeof callback === 'function' ? callback : function(){};
     var args = [configPath];
 
     stop();
@@ -25,13 +26,24 @@ function stop() {
     server = null;
 }
 
+function always (value){
+    return function (){
+        return value;
+    };
+}
+
+function getDoneFn (done, gruntContext){
+    var async = typeof gruntContext.async === 'function' ? gruntContext.async : always(done);
+    return async();
+}
+
 module.exports = function (grunt) {
     grunt.registerMultiTask('approvals-server', 'Run approvals http server', function (done) {
-        start(this.data.path);
+        start(this.data.path, getDoneFn(done, this));
     });
 
     grunt.registerMultiTask('approvals-server-start', 'Start approvals http server', function (done) {
-        start(this.data);
+        start(this.data, getDoneFn(done, this));
     });
 
     grunt.registerTask('approvals-server-stop', 'Start approvals http server', function () {
