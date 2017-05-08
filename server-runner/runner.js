10941506723127e2391cdca4ef27305940bb19cd
Chris Stead <cmstead@gmail.com>
2017-05-08 16:40:25 -0700

    Extracted runner to its own module

--- server-runner/runner.js
+++
@@ -1,6 +0,0 @@
-var path = require('path');
-
-var configPath = process.cwd() + path.sep + process.argv[2];
-var config = require(configPath);
-
-require('approvals-server')(config);
--- tasks/grunt-approvals-server.js
+++ tasks/grunt-approvals-server.js
@@ -1,30 +1,6 @@
 'use strict';
 
-var child = require('child_process');
-var path = require('path');
-
-var server = null;
-var runnerPath = [__dirname, '..', 'server-runner', 'runner'].join(path.sep);
-
-function start(configPath, callback) {
-    var cleanCallback = typeof callback === 'function' ? callback : function(){};
-    var args = [configPath];
-
-    stop();
-
-    server = child.fork(runnerPath, args);
-    server.disconnect();
-
-    setTimeout(callback, 250);
-}
-
-function stop() {
-    if (server !== null) {
-        server.kill();
-    }
-
-    server = null;
-}
+var runner = require('approvals-server-runner');
 
 function always (value){
     return function (){
